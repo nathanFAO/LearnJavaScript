20ec01cae9b2812450332146c96b47b9fdf53fb0
nathanFAO <nathan1fellipe@gmail.com>
2024-03-21 13:30:56 -0300

    TypeScript Basic and Terminology

---
+++ build/js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+//------------------
+//Basics
+//------------------
+// let username = 'rashi';
+// console.log(username);
+// let a: number = 12;
+// let b: number=  6;
+// let c: number = 2;
+// console.log(a / b)
+// console.log(c * b)
+//----------------------
+//Terminology
+//----------------------
+// let myName: string = 'rashi' 
+// let meaningOfLife: number
+// let isLoading: boolean
+// let album: number | string
+// isLoading = true
+// const sum = (a: number, b: string ) => {
+//     return a + b
+// }
+// let postId: string | number
+// let isActive: number | boolean
+// //RegExp = Regular Expression
+// let re: RegExp = /\w+/g
